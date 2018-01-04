da46ec3fc36d91b42d1a4427da86119f7da58ab1
Freymiller, Evan J <EVAN_J_FREYMILLER@homedepot.com>
2018-01-04 11:09:30 -0600

    changed color to purple

--- public/app/app.js
+++ public/app/app.js
@@ -5,7 +5,7 @@ $httpProvider.defaults.headers.post['Content-Type'] = 'text/plain';
 
   // For example: raised button text will be black instead of white.
   $mdThemingProvider.theme('default')
-    .primaryPalette('deep-orange', {
+    .primaryPalette('deep-purple', {
       'default': '400', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
@@ -13,7 +13,7 @@ $httpProvider.defaults.headers.post['Content-Type'] = 'text/plain';
     }).dark()
     // If you specify less than all of the keys, it will inherit from the
     // default shades
-    .accentPalette('teal', {
+    .accentPalette('deep-orange', {
       'default': '200' // use shade 200 for default, and keep all other shades the same
     });
     
