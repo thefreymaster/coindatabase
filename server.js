7623352dfb9aa588f10e0577342878de161aace2
Freymiller, Evan J <EVAN_J_FREYMILLER@homedepot.com>
2018-01-08 16:24:46 -0600

    flipped name and symbol sidenav

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
-var port = 5700;
+var port = 5800;
 app.listen(process.env.PORT || port, function () { 
     console.log('Running REST HTTPS server on port: '+port);
 });
