8e0740ac2451e1d7a6f9c908ca7784c7f3d6682a
Freymiller, Evan J <EVAN_J_FREYMILLER@homedepot.com>
2018-01-11 13:03:56 -0600

    changed to monochrome style

--- public/app/app.js
+++ public/app/app.js
@@ -5,12 +5,12 @@ var app = angular.module('coindbApp', ['ui.router', 'ngMaterial', 'ngMessages', 
 
   // For example: raised button text will be black instead of white.
   $mdThemingProvider.theme('default')
-    .primaryPalette('deep-purple', {
+    .primaryPalette('grey', {
       'default': '400', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
-    }).dark()
+    })
     // If you specify less than all of the keys, it will inherit from the
     // default shades
     .accentPalette('deep-orange', {
